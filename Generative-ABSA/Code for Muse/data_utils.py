8c320fb8b2cdf50f6b4967e1b4b75e78b2f20c2d
AtiUsm <48758225+AtiUsm@users.noreply.github.com>
2024-03-26 20:48:58 +0000

    Add files via upload

---
+++ Generative-ABSA/Code for Muse/data_utils.py
@@ -0,0 +1,147 @@
+# This file contains all data loading and transformation functions
+
+import time
+from torch.utils.data import Dataset
+import pandas as pd
+
+senttag2word = {'pos': 'positive', 'neg': 'negative', 'neu': 'neutral'}
+def get_extraction_aste_targets(data_path):
+    df= pd.read_csv(data_path)
+    ids=df['id']
+    ids=list(ids)
+    ids=set(ids)
+    label_dict=[]
+    flag=0
+    label=''
+    context=[]
+    for id in ids:
+        sf=df.loc[df['id']==id]
+        s_ids=sf['segment_id']
+        s_id=list(s_ids)
+        s_ids=set(s_id)
+        for s_id in s_ids:
+            nf=sf.loc[sf['segment_id']==s_id]
+            flag=0
+            for ind in nf.index:
+
+                if flag==0:
+                    text=nf['text'][ind]
+                    curr_id=str(id)+'-'+str(s_id)
+                    label=''
+                    if nf["aspect"][ind]!="-":
+                        label+='(' + nf["aspect"][ind] +', '+ nf["opinion"][ind]+', '+ senttag2word[nf["sentiment"][ind]]+')'
+                    else:
+                        label= "(None" +','+ "None" +','+ "None)"
+
+                if flag==1:
+                    if nf["aspect"][ind]!="-":
+                        #print(nf["sentiment"][ind])
+                        label=label+'; ('+nf["aspect"][ind] +', '+ nf["opinion"][ind]+', '+ senttag2word[nf["sentiment"][ind]]+')'
+                flag=1
+            label_dict.append({curr_id:label})
+            context.append({curr_id:text})
+    targets=[v  for i in label_dict  for k,v in i.items()]
+    inputs=[c.split()  for i in context  for k,c in i.items()]
+    sents=[c  for i in context  for k,c in i.items()]
+    return sents, inputs, targets
+
+def get_transformed_io(data_path, paradigm, task):
+    """
+    The main function to transform the Input & Output according to 
+    the specified paradigm and task
+    """
+    #sents, labels = read_line_examples_from_file(data_path)
+
+    # the input is just the raw sentence
+    #inputs = [s.copy() for s in sents]
+
+    # Get target according to the paradigm
+    # annotate the sents (with label info) as targets
+    # directly treat label infor as the target
+    if paradigm == 'extraction':
+        if task == 'aste':
+            sents,inputs,targets = get_extraction_aste_targets(data_path)
+        else:
+            raise NotImplementedError
+    else:
+        print('Unsupported paradigm!')
+        raise NotImplementedError 
+
+    return sents, inputs, targets
+
+
+class ABSADataset(Dataset):
+    def __init__(self, tokenizer, data_dir, data_type, paradigm, task, max_len=128):
+        # 'data/aste/rest16/train.txt'
+        self.data_path = f'data/{task}/{data_dir}/{data_type}.csv'
+        self.paradigm = paradigm
+        self.task = task
+        self.max_len = max_len
+        self.tokenizer = tokenizer
+        
+        self.inputs = []
+        self.targets = []
+
+        self._build_examples()
+
+    def __len__(self):
+        return len(self.inputs)
+
+    def __getitem__(self, index):
+        source_ids = self.inputs[index]["input_ids"].squeeze()
+        target_ids = self.targets[index]["input_ids"].squeeze()
+
+        src_mask = self.inputs[index]["attention_mask"].squeeze()      # might need to squeeze
+        target_mask = self.targets[index]["attention_mask"].squeeze()  # might need to squeeze
+
+        return {"source_ids": source_ids, "source_mask": src_mask, 
+                "target_ids": target_ids, "target_mask": target_mask}
+
+    def _build_examples(self):
+
+        sents, inputs, targets = get_transformed_io(self.data_path, self.paradigm, self.task)
+
+        for i in range(len(inputs)):
+
+            input = ' '.join(inputs[i]) 
+            target = targets[i]
+
+            tokenized_input = self.tokenizer.batch_encode_plus(
+              [input], max_length=self.max_len, padding='max_length', truncation=True,
+              return_tensors="pt",
+            )
+            tokenized_target = self.tokenizer.batch_encode_plus(
+              [target], max_length=self.max_len, padding='max_length', truncation=True,
+              return_tensors="pt"
+            )
+            #print(len(tokenized_input)
+            #print(len(tokenized_target)
+
+            self.inputs.append(tokenized_input)
+            self.targets.append(tokenized_target)
+
+def write_results_to_log(log_file_path, args, dev_results, global_steps):
+    """
+    Record dev and test results to log file
+    """
+    local_time = time.asctime(time.localtime(time.time()))
+
+    train_settings = "Train setting: bs={0}, lr={1}, num_epochs={2}".format(
+        args.train_batch_size, args.learning_rate, args.num_train_epochs
+    )
+    results_str = "\n* Results *:  Dev  \n"
+
+    metric_names = ['f1', 'precision', 'recall']
+    for gstep in global_steps:
+        results_str += f"Step-{gstep}:\n"
+        for name in metric_names:
+            name_step = f'{name}_{gstep}'
+            results_str += f"{name:<8}: {dev_results[name_step]:.4f}"
+            results_str += ' '*5
+        results_str += '\n'
+
+    exp_settings = f"{args.task} on {args.dataset} under {args.paradigm}; Train bs={args.train_batch_size}, num_epochs = {args.num_train_epochs}"
+    log_str = f"{local_time}\n{exp_settings}\n{results_str}\n\n"
+
+    with open(log_file_path, "a+") as f:
+        f.write(log_str)
---
+++ Generative-ABSA/Code for Muse/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+python main.py --task aste \
+            --dataset muse \
+            --model_name_or_path t5-base \
+            --paradigm extraction \
+            --n_gpu 0 \
+            --do_train\
+            --do_eval\
+            --train_batch_size 12\
+            --gradient_accumulation_steps 1 \
+            --eval_batch_size 12 \
+            --learning_rate 3e-4 \
+            --num_train_epochs 20 \
