fd7f7ce4eda619d8c534adfbf95374164ad1ef80
AtiUsm <48758225+AtiUsm@users.noreply.github.com>
2024-11-15 16:34:51 +0000

    Update demo.py

--- demo/demo.py
+++ demo/demo.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from annotated_text import annotated_text
+from streamlit_extras.mention import mention
 #from st_annotated_text import annotated_text
 st.title(':blue[MuSe-CarASTE: A comprehensive dataset for aspect sentiment triplet extraction in automotive review videos]')
 st.title(':green[Get a sneak-peek into our dataset! :eyes:]')
